ef8b100b2d3919a82944979d77e224f2b8b493ff
johnmcraig <jmcraig32884@gmail.com>
2017-09-05 00:32:14 -0400

    Added js file and code

---
+++ script.js
@@ -0,0 +1,19 @@
+var inputText = document.getElementById("text-field"),
+    printButton = document.getElementById("print-button"),
+    clearButton = document.getElementById("clear-button"),
+    printArea = document.getElementById("print-area"),
+    printFizz = document.getElementById("fizz-buzz");
+
+/*
+printButton.addEventListener("click", function () {
+    printArea.innerText += "\n" + "Hello, " + inputText.value;
+});
+
+clearButton.addEventListener("click", function () {
+    printArea.innerHTML = "";
+});
+*/
+
+printButton.addEventListener("click", function () {
+    alert(5 + 7);
+});
